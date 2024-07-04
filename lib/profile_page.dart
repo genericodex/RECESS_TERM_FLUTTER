9532f9d99ddb4efb8ba46d9100784b21448cf964
Ssemwanga <smahadicon@gmail.com>
2024-07-04 16:59:07 +0300

    profile

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -2,114 +2,114 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
-class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
-
-  @override
-  _ProfilePageState createState() => _ProfilePageState();
-}
-
-class _ProfilePageState extends State<ProfilePage> {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _phoneController = TextEditingController();
-  File? _image;
-
-  Future<void> _pickImage() async {
-    final ImagePicker picker = ImagePicker();
-    final XFile? pickedImage = await picker.pickImage(source: ImageSource.gallery);
-    if (pickedImage != null) {
-      setState(() {
-        _image = File(pickedImage.path);
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Edit Profile'),
-        backgroundColor: Colors.green,
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.green, Colors.red, Colors.black],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Stack(
-                  children: [
-                    CircleAvatar(
-                      radius: 60,
-                      backgroundColor: Colors.white,
-                      backgroundImage: _image != null ? FileImage(_image!) : null,
-                      child: _image == null
-                          ? const Icon(Icons.person, size: 60, color: Colors.grey)
-                          : null,
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: IconButton(
-                        icon: const Icon(Icons.camera_alt, color: Colors.black),
-                        onPressed: _pickImage,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: _nameController,
-                decoration: const InputDecoration(
-                  labelText: 'Name',
-                  filled: true,
-                  fillColor: Colors.white70,
-                ),
-              ),
-              const SizedBox(height: 10),
-              TextField(
-                controller: _emailController,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  filled: true,
-                  fillColor: Colors.white70,
-                ),
-              ),
-              const SizedBox(height: 10),
-              TextField(
-                controller: _phoneController,
-                decoration: const InputDecoration(
-                  labelText: 'Phone Number',
-                  filled: true,
-                  fillColor: Colors.white70,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Save profile data
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                  ),
-                  child: const Text('Save Changes'),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class ProfilePage extends StatefulWidget {
+//   const ProfilePage({super.key});
+//
+//   @override
+//   _ProfilePageState createState() => _ProfilePageState();
+// }
+//
+// class _ProfilePageState extends State<ProfilePage> {
+//   final TextEditingController _nameController = TextEditingController();
+//   final TextEditingController _emailController = TextEditingController();
+//   final TextEditingController _phoneController = TextEditingController();
+//   File? _image;
+//
+//   Future<void> _pickImage() async {
+//     final ImagePicker picker = ImagePicker();
+//     final XFile? pickedImage = await picker.pickImage(source: ImageSource.gallery);
+//     if (pickedImage != null) {
+//       setState(() {
+//         _image = File(pickedImage.path);
+//       });
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Edit Profile'),
+//         backgroundColor: Colors.green,
+//       ),
+//       body: Container(
+//         padding: const EdgeInsets.all(16.0),
+//         decoration: BoxDecoration(
+//           gradient: LinearGradient(
+//             colors: [Colors.green, Colors.red, Colors.black],
+//             begin: Alignment.topLeft,
+//             end: Alignment.bottomRight,
+//           ),
+//         ),
+//         child: SingleChildScrollView(
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Center(
+//                 child: Stack(
+//                   children: [
+//                     CircleAvatar(
+//                       radius: 60,
+//                       backgroundColor: Colors.white,
+//                       backgroundImage: _image != null ? FileImage(_image!) : null,
+//                       child: _image == null
+//                           ? const Icon(Icons.person, size: 60, color: Colors.grey)
+//                           : null,
+//                     ),
+//                     Positioned(
+//                       bottom: 0,
+//                       right: 0,
+//                       child: IconButton(
+//                         icon: const Icon(Icons.camera_alt, color: Colors.black),
+//                         onPressed: _pickImage,
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//               const SizedBox(height: 20),
+//               TextField(
+//                 controller: _nameController,
+//                 decoration: const InputDecoration(
+//                   labelText: 'Name',
+//                   filled: true,
+//                   fillColor: Colors.white70,
+//                 ),
+//               ),
+//               const SizedBox(height: 10),
+//               TextField(
+//                 controller: _emailController,
+//                 decoration: const InputDecoration(
+//                   labelText: 'Email',
+//                   filled: true,
+//                   fillColor: Colors.white70,
+//                 ),
+//               ),
+//               const SizedBox(height: 10),
+//               TextField(
+//                 controller: _phoneController,
+//                 decoration: const InputDecoration(
+//                   labelText: 'Phone Number',
+//                   filled: true,
+//                   fillColor: Colors.white70,
+//                 ),
+//               ),
+//               const SizedBox(height: 20),
+//               Center(
+//                 child: ElevatedButton(
+//                   onPressed: () {
+//                     // Save profile data
+//                   },
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor: Colors.green,
+//                   ),
+//                   child: const Text('Save Changes'),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
